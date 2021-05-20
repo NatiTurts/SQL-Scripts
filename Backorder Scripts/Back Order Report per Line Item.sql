8ed5d5075fbd60f287709da365d96e73e0c11f8e
Nati Turtledove <71794006+NatiTurts@users.noreply.github.com>
2021-05-20 09:44:54 +0200

    Update Back Order Report per Line Item.sql

--- Backorder Scripts/Back Order Report per Line Item.sql
+++ Backorder Scripts/Back Order Report per Line Item.sql
@@ -15,8 +15,8 @@ select
 , Datename(month,p.OrderDate) as Month
 , CAST(MONTH(p.OrderDate) AS VARCHAR(4))[MonthNo]
 , CAST(YEAR(p.OrderDate) AS VARCHAR(4))[Year]
-from ICC_NEW.dbo._bvSalesOrdersFull P
-left join ICC_NEW.dbo._bvCMCustomerFull C on C.Customer = P.Account
+from .dbo._bvSalesOrdersFull P
+left join .dbo._bvCMCustomerFull C on C.Customer = P.Account
 where C.Customer not in ('ALL030', 'TJO002', 'TOY001')
 and P.On_Hold = '0' and P.bCODAccount = '0' and P.DocumentStateDesc in ('Partially Processed', 'Unprocessed', 'Unprocessed and Patrcially')
 and P.QtyOutstanding >0 and  (P.OrderDate>=@DateStart@) and (P.OrderDate<=@DateEnd@)
