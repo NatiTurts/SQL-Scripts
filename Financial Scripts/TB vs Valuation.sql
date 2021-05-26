c126bba5d46d9383eb8c72e1b7428b0f856bfd7f
Nati Turtledove <nati3280@gmail.com>
2021-05-26 19:00:51 +0200

    commit

---
+++ Financial Scripts/TB vs Valuation.sql
@@ -0,0 +1,28 @@
+use tempdb
+
+	If(OBJECT_ID('tempdb..#InvStaging') Is Not Null)
+	Begin
+		Drop Table #InvStaging
+	End	
+
+	
+		create table #InvStaging (ItemCode nvarchar (max),ItemGroup nvarchar (max), Valuation float)
+		--create table #Final (FinalAudit varchar (max), Checks int)	
+
+			Insert into #InvStaging (ItemCode, ItemGroup, Valuation)
+				select
+				S.Code
+				, S.ItemGroup
+				, [Weighted Value] =  Round((S.Qty_On_Hand*S.AveUCst),2)
+				from ICC_TEST.dbo.StkItem S
+
+select
+INS.ItemGroup
+, SUM(INS.Valuation) as Valuation
+from #InvStaging INS
+group by INS.ItemGroup
+
+	If(OBJECT_ID('tempdb..#InvStaging') Is Not Null)
+	Begin
+		Drop Table #InvStaging
+	End	
