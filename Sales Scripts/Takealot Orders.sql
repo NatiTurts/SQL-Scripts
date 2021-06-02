32b9b7965e3604defdb623b5229761ad1f5343d6
Nati Turtledove <nati3280@gmail.com>
2021-06-02 13:06:02 +0200

    commit

---
+++ Sales Scripts/Takealot Orders.sql
@@ -0,0 +1,36 @@
+SELECT 
+  C.Account
+, P.Reference AS [Invoice Number]
+, P.Order_No AS [Order Number]
+, P.ExtOrderNum as [External Order Number]
+, S.Code AS ItemCode
+, S.Description_1 AS ItemDescription
+, C.PriceListName AS Pricelist
+, P.Id as [Transaction Type]
+, P.TxDate AS Date
+, SUM(P.ActualQuantityInvoiced) AS Quantity
+, PL.fExclPrice as [Excl]
+, PL.fInclPrice as [Incl]
+, SUM(P.ActualValue) AS LineSales
+, Case when (PL.fInclPrice * P.ActualQuantityInvoiced) = P.ActualValue then 'True' else 'False' end as [Check]
+FROM ICC_NEW.dbo._bvSTTransactionsFull AS P
+LEFT JOIN ICC_NEW.dbo._bvARAccountsFull AS C ON C.DCLink = P.DrCrAccount
+LEFT JOIN ICC_NEW.dbo._bvCMCustomerFull AS D ON D.Customer = C.Account
+LEFT JOIN ICC_NEW.dbo._bvStockFull AS S ON S.StockLink = P.AccountLink
+Inner join ICC_NEW.dbo._evPriceListPrices PL on PL.iStockID = P.AccountLink
+WHERE (P.Id IN ('Inv', 'Crn', 'OInv', 'POSI', 'POSC')) and PL.iPriceListNameID = '11'
+AND (P.TxDate >= @StartDate) AND (P.TxDate <= @EndDate) and P.Account = 'TAK006'
+group by
+ C.Account
+, P.Reference
+, P.Order_No
+, P.ExtOrderNum
+, S.Code
+, S.Description_1
+, C.PriceListName
+, P.TxDate
+, PL.fExclPrice
+, PL.fInclPrice
+, P.ActualQuantityInvoiced
+, P.ActualValue
+, P.Id
